f1c7bb61ed9df353ff5508afbe9d2d2123f49184
MajdRmd <majdramadan7@gmail.com>
2025-02-19 15:24:12 +0200

    Part A

---
+++ nabiha.py
@@ -0,0 +1,9 @@
+salary = int(input('Enter your salary \n'))
+month = input('Enter the month of the salary\n')
+savings = int(input('Enter amount of savings (in percentage)\n'))
+rent = int(input('Enter amount of rent (in percentage) \n'))
+electricity = int(input('Enter amount of electricity (in percentage) \n'))
+
+amountofsavings = salary*savings/100
+amountofrent = rent*savings/100
+amountofelectricity = electricity*savings/100
